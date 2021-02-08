99fea5d9ac6996f1ef722115ab2a30a98e1f24e4
abriciof <fabricio15.guimaraes@gmail.com>
2021-02-08 16:23:48 -0400

    08-02

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 
+// Carregamento Inicial da página
+document.getElementById('customRangeCorrente').disabled = true;
+document.getElementById('customRangeTensao').disabled = false;        
+document.getElementById('customRangeResistencia').disabled = false;
+
+
+
+
+// Depois de escolher uma variável
+function bloquear(value){
+    if(value == 1){
+        document.getElementById('customRangeCorrente').disabled = true;
+        document.getElementById('customRangeTensao').disabled = false;        
+        document.getElementById('customRangeResistencia').disabled = false;
+
+    }else if(value == 2 ){
+        document.getElementById('customRangeCorrente').disabled = false;
+        document.getElementById('customRangeTensao').disabled = true;        
+        document.getElementById('customRangeResistencia').disabled = false;
+
+    }else if(value == 3){
+        document.getElementById('customRangeCorrente').disabled = false;
+        document.getElementById('customRangeTensao').disabled = false;        
+        document.getElementById('customRangeResistencia').disabled = true;
+    }
+
+}
+
 
 // const rangers = document.querySelectorAll('.custom-range');
 // let t = document.getElementById('customRangeTensao');
@@ -19,11 +47,12 @@ function calcular(){
 
     if(menu.value == 1){ // Corrente
         const range = document.getElementById('customRangeCorrente');
+
     
         if(tensao.value!=0 && resistencia.value!=0){
             resultado = tensao.value/resistencia.value;
             resultado *=1000;
-            resultado = resultado.toFixed(2);
+            resultado = resultado.toFixed(1);
             corrente.value = resultado;
             range.value = resultado;
         }
@@ -35,7 +64,7 @@ function calcular(){
             resultado = corrente.value*resistencia.value;
             resultado /= 10000;
         
-            resultado = resultado.toFixed(2);
+            resultado = resultado.toFixed(1);
             tensao.value = resultado;
             range.value = resultado;
         }
@@ -48,7 +77,7 @@ function calcular(){
             resultado = (tensao.value)/(corrente.value);
             resultado *= 100000;
            
-            resultado = resultado.toFixed(2);
+            resultado = resultado.toFixed(1);
             resistencia.value = resultado;
             range.value = resultado;
         }
